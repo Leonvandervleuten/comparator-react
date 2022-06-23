823ddd68932d3b2773b81f702587677d4e5d81dd
Leon van der Vleuten <Leonvandervleuten@gmail.com>
2022-06-23 16:32:27 +0200

    Upload and post csv file working

--- src/components/Uploadxls/Uploadxls.jsx
+++ src/components/Uploadxls/Uploadxls.jsx
@@ -4,40 +4,72 @@ import axios from "axios";
 
 const Uploadxls = () => {
 
-    const [selectedFile, setSelectedFile] = useState(null);
+    const [selectedFile, setSelectedFile] = useState([]);
 
 
     const onFileChange = (event) => {
-        // Update the state
-        setSelectedFile({selectedFile: event.target.files[0]})
+
+        const files = event.target.files;
+
+        for (let i = 0; i < files.length; i++) {
+            setSelectedFile(selectedFile => [...selectedFile, {selectedFile: event.target.files[i]}])
+        }
+
     };
 
     const onFileUpload = () => {
+        for (let i = 0; i < selectedFile.length; i++){
+            const formData = new FormData();
 
-        // Create an object of formData
-        const formData = new FormData();
+            const file = selectedFile[i];
 
-        // Update the formData object
-        formData.append(
-            "csv",
-            selectedFile.selectedFile,
-            selectedFile.selectedFile.name
-        );
+            formData.append(
+                "csv",
+                file.selectedFile,
+                file.selectedFile.name
+            )
 
-        // Details of the uploaded file
-        console.log(selectedFile);
+            console.log(file);
 
-        // Request made to the backend api
-        // Send formData object
-        axios.post("http://localhost:8080/compare", formData);
+            axios.post("http://localhost:8080/compare", formData)
+        }
+
+        //
+        // // Create an object of formData
+        // const formData = new FormData();
+        //
+        // // Update the formData object
+        // formData.append(
+        //     "csv",
+        //     selectedFile.selectedFile,
+        //     selectedFile.selectedFile.name
+        // );
+        //
+        // // Details of the uploaded file
+        // console.log(selectedFile);
+        //
+        // // Request made to the backend api
+        // // Send formData object
+        // axios.post("http://localhost:8080/compare", formData);
     };
 
     const fileData = () => {
         if (selectedFile) {
+
             return (
                 <div>
-                    <h3>File Details:</h3>
-                    <p>File Name: {selectedFile.selectedFile.name}</p>
+                    <div>
+                        <h4>Files:</h4>
+                    </div>
+
+                    <div>
+                        {selectedFile
+                            .map((item) => {
+                                return (
+                                    <div>{item.selectedFile.name}</div>
+                                )
+                            })}
+                    </div>
                 </div>
             );
         } else {
@@ -59,7 +91,7 @@ const Uploadxls = () => {
             </h3>
             <label className={"file-upload"}>
                 <input
-                    type="file" onChange={onFileChange}/>
+                    type="file" multiple={true} onChange={onFileChange}/>
                 Browse
             </label>
             {fileData()}
